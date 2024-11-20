c63db52e1414a43620b0e3401c298c91b5bf28c8
ridvanKarsli <148614751+ridvanKarsli@users.noreply.github.com>
2024-11-20 17:24:55 +0300

    Initial Commit

---
+++ FindItGame/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FindItGame
+//
+//  Created by Rıdvan Karslı on 20.11.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ FindItGameTests/FindItGameTests.swift
@@ -0,0 +1,17 @@
+//
+//  FindItGameTests.swift
+//  FindItGameTests
+//
+//  Created by Rıdvan Karslı on 20.11.2024.
+//
+
+import Testing
+@testable import FindItGame
+
+struct FindItGameTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
